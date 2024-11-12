b5389470574b067053ee07e42c56fc76133d3910
Payson Eberz <63822744+PAYS0N@users.noreply.github.com>
2024-11-12 11:09:46 -0600

    Set up file structure

---
+++ lib/hashmap.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module OdinHashmap
+  # string hashmap implementation
+  class Hashmap
+    def initialize
+      puts "initialized"
+    end
+  end
+end
---
+++ lib/odin_hashmap.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+require_relative("hashmap")
+
+OdinHashmap::Hashmap.new
